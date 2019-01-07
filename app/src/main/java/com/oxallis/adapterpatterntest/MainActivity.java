6814285ef9a4dcccadbec930970589b3c9353001
hadi-mohamadi <work.hadimohammadi@gmail.com>
2019-01-07 12:22:44 +0330

    initialize commit

---
+++ app/src/main/java/com/oxallis/adapterpatterntest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.oxallis.adapterpatterntest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oxallis.adapterpatterntest.adapterrequirments.SocketAdapter;
+import com.oxallis.adapterpatterntest.adapterrequirments.SocketClassAdapterImpl;
+import com.oxallis.adapterpatterntest.adapterrequirments.SocketObjectAdapterImpl;
+import com.oxallis.adapterpatterntest.adapterrequirments.Volt;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testClassAdapter();
+        testObjectAdapter();
+    }
+
+
+    private static void testObjectAdapter() {
+        SocketAdapter sockAdapter = new SocketObjectAdapterImpl();
+        Volt v3 = getVolt(sockAdapter,3);
+        Volt v12 = getVolt(sockAdapter,12);
+        Volt v120 = getVolt(sockAdapter,120);
+        System.out.println("v3 volts using Object Adapter="+v3.getVolts());
+        System.out.println("v12 volts using Object Adapter="+v12.getVolts());
+        System.out.println("v120 volts using Object Adapter="+v120.getVolts());
+    }
+
+    private static void testClassAdapter() {
+        SocketAdapter sockAdapter = new SocketClassAdapterImpl();
+        Volt v3 = getVolt(sockAdapter,3);
+        Volt v12 = getVolt(sockAdapter,12);
+        Volt v120 = getVolt(sockAdapter,120);
+        System.out.println("v3 volts using Class Adapter="+v3.getVolts());
+        System.out.println("v12 volts using Class Adapter="+v12.getVolts());
+        System.out.println("v120 volts using Class Adapter="+v120.getVolts());
+    }
+
+    private static Volt getVolt(SocketAdapter sockAdapter, int i) {
+        switch (i){
+            case 3: return sockAdapter.get3Volt();
+            case 12: return sockAdapter.get12Volt();
+            case 120: return sockAdapter.get120Volt();
+            default: return sockAdapter.get120Volt();
+        }
+    }
+}
---
+++ app/src/main/java/com/oxallis/adapterpatterntest/adapterrequirments/Socket.java
@@ -0,0 +1,8 @@
+package com.oxallis.adapterpatterntest.adapterrequirments;
+
+public class Socket {
+
+    public Volt getVolt(){
+        return new Volt(120);
+    }
+}
